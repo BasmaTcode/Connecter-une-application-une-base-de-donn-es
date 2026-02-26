a2af3d7bae5100a516f54a1d32f379dea09119e5
BasmaTcode <basma21trid@gmail.com>
2026-02-26 10:21:10 +0000

    add

--- Chapitre3/actions_utilisateurs.php
+++ Chapitre3/actions_utilisateurs.php
@@ -1,23 +1,23 @@
 <?php
 require 'connexion.php';
 
-try {
-    $stmt = $pdo->prepare("INSERT INTO Utilisateur (nom, email) VALUES (:nom, :email)");
-    $stmt->execute([
+try { 
+    $stmt = $pdo->prepare("INSERT INTO Utilisateur (nom, email) VALUES (:nom, :email)"); // hna bhal kanqolo wjdli had tableau  , u aneatik values mn baed
+    $stmt->execute([ // kaneatiw values f array
         'nom' => 'Safa',
         'email' => 'ehsafaa7@gmail.com'
     ]);
     echo "Utilisateur ajouté avec succès.";
-} catch (PDOException $e) {
+} catch (PDOException $e) { // ida wqae ghalat f try , catch kietina exception w yhuta f variable $e
     echo "Erreur : " . $e->getMessage();
 }
-$stmt = $pdo->prepare("UPDATE Utilisateur SET email = :email WHERE id = :id");
-$stmt->execute([
+$stmt = $pdo->prepare("UPDATE Utilisateur SET email = :email WHERE id = :id"); // hna bhal kanqolo update had tableau w 3tina condition bash n3rfou achno nbdlou
+$stmt->execute([ // kaneatiw values f array
     'email' => 'ehsafaa5@gmail.com',
     'id' => 3
 ]);
 echo "Utilisateur mis à jour.";
-$stmt = $pdo->prepare("DELETE FROM Utilisateur WHERE id = :id");
+$stmt = $pdo->prepare("DELETE FROM Utilisateur WHERE id = :id"); // hna  kanqolo delete had tableau w 3tina condition bash n3rfu achnu nhaydu
 $stmt->execute(['id' => 3]);
 echo "Utilisateur supprimé.";
-echo $stmt->rowCount() . " ligne(s) affectée(s).";
+echo $stmt->rowCount() . " ligne(s) affectée(s)."; // Hna kanshufu uash dik operation t executat
--- Chapitre4.php
+++ Chapitre4.php
@@ -1,27 +1,23 @@
 <?php
 
 try {
-    $pdo = new PDO('mysql:host=localhost;dbname=blogdb', 'root', '');
+    $pdo = new PDO('mysql:host=localhost;dbname=blogdb', 'root', ''); // Hna anbdaw connexion b base de données
 
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // ida wqae ghalat emlna exception bash nqdro nthakmo feh
 
-    echo "Connexion réussie";
+    echo "Connexion réussie"; // ida kant connexion najha kitlaena had msg
 
-} catch (PDOException $e) {
+} catch (PDOException $e) { // ida wqae ghalat f try , catch kietina exception w yhuta f variable $e
 
-    echo "Erreur de connexion : " . $e->getMessage();
+    echo "Erreur de connexion : " . $e->getMessage(); // hna kijiblna l message dl ghalat
 }
 try {
-    $pdo->query("SELECT * FROM table_inexistante");
+    $pdo->query("SELECT * FROM table_inexistante"); // hna kanhawlu njibu infos mn uahd tableau makaynashi
 } catch (PDOException $e) {
-    echo "Erreur SQL : " . $e->getMessage();
+    echo "Erreur SQL : " . $e->getMessage(); // ida kan khata2 ayban message dial erreur
 }
-try {
-    $pdo->query("SELECT * FROM table_inexistante");
-} catch (PDOException $e) {
-    echo "Erreur SQL : " . $e->getMessage();
-}
+
 catch (PDOException $e) {
-    file_put_contents('erreurs.log', $e->getMessage(), FILE_APPEND);
+    file_put_contents('erreurs.log', $e->getMessage(), FILE_APPEND); // hna kanhutu message dl erreur f file ismu "erreurs.log" , bash maybanush les details l users
     echo "Une erreur est survenue. Contactez l'administrateur.";
 }
